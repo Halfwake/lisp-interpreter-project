ee4e9bd2162ed7450960f5866e061b4660c1637d
Andrew Dudash <drewd8@vt.edu>
2017-02-07 14:44:31 -0500

    Update tests to include exception throwing code.

--- test_environment.cpp
+++ test_environment.cpp
@@ -10,4 +10,10 @@ int main() {
   env.set("work", 3);
   std::cout << "Set [work] to [3]." << std::endl;
   std::cout << "Work was [" << env.get("work") << "]" << std::endl;
+  try {
+    std::cout << "Trying to retrieve a value that doesn't exist." << std::endl;
+    env.get("nothere");
+  } catch (LookupException err) {
+    std::cout << "Tried to lookup [" << err.getSymbol() << "] and failed." << std::endl;
+  }
 }
