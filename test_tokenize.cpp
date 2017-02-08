abb6e2f1d84034d1e8d3baaac9c5e78644a661aa
Andrew Dudash <drewd8@vt.edu>
2017-02-07 22:42:38 -0500

    Add a test to tokenize tests for the typical case.

--- test_tokenize.cpp
+++ test_tokenize.cpp
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 #define TOKENIZE "[Tokenize]"
 
@@ -16,4 +17,11 @@ TEST_CASE("Test empty stream.", TOKENIZE) {
   std::stringstream stream;
   std::vector<Token> tokens = tokenize(stream);
   REQUIRE(tokens.empty());
+}
+
+TEST_CASE("Test typical case.", TOKENIZE) {
+  std::stringstream stream("(+ 12 34 (* 56 78))");
+  std::vector<Token> tokens = tokenize(stream);
+  std::vector<Token> expected = { "(", "12", "34", "(", "56", "78", ")", ")" };
+  REQUIRE(std::equal(tokens.begin(), tokens.end(), expected.begin()));
 }
