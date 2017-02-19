dc10c5da65975ce54cb309acbb6fc5cc66c665bd
Andrew Dudash <drewd8@vt.edu>
2017-02-18 23:09:16 -0500

    Add test for number tokens.

--- test_expression.cpp
+++ test_expression.cpp
@@ -47,5 +47,6 @@ TEST_CASE("Match number tokens.", MATCH) {
   REQUIRE(match_number(Token(ATOM, "314", 1)));
   REQUIRE(match_number(Token(ATOM, "3.14", 1)));
   REQUIRE(match_number(Token(ATOM, "314.", 1)));
+  REQUIRE_FALSE(match_number(Token(ATOM, "abc", 1)));
   REQUIRE_FALSE(match_number(Token(ATOM, ".3", 1)));
 }
